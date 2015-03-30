dd9cdedb29ba78eebb155f92d8c3db3367501a81
Brandon Norris <blnorris@gmail.com>
2015-03-30 05:39:47 +0000

    DEAL WITH IT

--- gamemanager/receive-test-circuits.py
+++ gamemanager/receive-test-circuits.py
@@ -8,7 +8,7 @@ class helloEv(Event):
     """hello Event"""
 
 class SerInput(Component):
-    def __init__(self, device = '/dev/ttyUSB0'):
+    def __init__(self, device = '/dev/ttYACM0'):
         super(SerInput, self).__init__()
         Serial(device).register(self)
     
@@ -25,6 +25,6 @@ class SerInput(Component):
 
 if __name__ == "__main__":        
 
-    (SerInput(device = '/dev/ttyUSB0') + Debugger()).run()
+    (SerInput(device = '/dev/ttyACM0') + Debugger()).run()
     ''' + Debugger() + TT()
     d.run()'''
